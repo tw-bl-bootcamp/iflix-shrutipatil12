6e35388ce14cd5d6384bebdd32c31be3fc2f7f76
shrutidhananjaypatil <shrutidpatil12@gmail.com>
2019-09-12 16:44:19 +0530

    [Shruti] Add. check behaviour of login api for invalid email id

--- Server/Test/loginTest.spec.js
+++ Server/Test/loginTest.spec.js
@@ -5,23 +5,18 @@ const chaihttp = require('chai-http');
 
 chai.use(chaihttp);
 
-<<<<<<< HEAD
-describe('Given wrong password while login When Pass To LogIn API', () => {
-=======
 describe('Given wrong login credentials When Pass To LogIn API', () => {
->>>>>>> 20b9f762dd54c70f2bbd443d2dc1ed3a3814a002
     it('It will return Status Code 422', (done) => {
         let userData = {
             email: "wronginput@gmail.com",
             password: "ab"
         }
-        chai.request(server).post('/login').send(userData).end((err,res) => {
-                assert.equal(422, res.status);
-                done();
-            })
+        chai.request(server).post('/login').send(userData).end((err, res) => {
+            assert.equal(422, res.status);
+            done();
+        })
     })
-})  
-<<<<<<< HEAD
+})
 
 describe('Given wrong format of email while login When Pass To LogIn API', () => {
     it('It will return Status Code 422', (done) => {
@@ -29,12 +24,9 @@ describe('Given wrong format of email while login When Pass To LogIn API', () =>
             email: "wronginput@",
             password: "abc@ABC12"
         }
-        chai.request(server).post('/login').send(userData).end((err,res) => {
-                assert.equal(422, res.status);
-                done();
-            })
+        chai.request(server).post('/login').send(userData).end((err, res) => {
+            assert.equal(422, res.status);
+            done();
+        })
     })
-})  
-=======
->>>>>>> 20b9f762dd54c70f2bbd443d2dc1ed3a3814a002
- 
+})
