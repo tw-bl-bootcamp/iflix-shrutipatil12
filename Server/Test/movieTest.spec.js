b91e1296afd3cd8c47f99b66661c8cd5a81a74bf
shrutidhananjaypatil <shrutidpatil12@gmail.com>
2019-09-13 16:52:34 +0530

    [Shruti] Add. check the behaviour of movie show api for invalid request url

--- Server/Test/movieTest.spec.js
+++ Server/Test/movieTest.spec.js
@@ -18,3 +18,33 @@ describe('Given empty request Url for movieShow API', () => {
         })
     })
 })
+
+describe('Given correct request Url for movieShow API', () => {
+    it('It will return Status Code 200', (done) => {
+        let movieData = {
+            title: "Avengers",
+            rating: "G",
+            review:"4.5"
+        }
+        chai.request(server).get('/movieShows').send(movieData).end((err, res) => {
+            assert.equal(200, res.status);
+            done();
+        })
+    })
+})
+
+describe('Given empty request Url for movieShow API', () => {
+    it('It will return Status Code 404', (done) => {
+        let movieData = {
+            title: "Avengers",
+            rating: "G",
+            review:"4.5"
+        }
+        chai.request(server).get('/Shows').send(movieData).end((err, res) => {
+            assert.equal(404, res.status);
+            done();
+        })
+    })
+})
+
+
